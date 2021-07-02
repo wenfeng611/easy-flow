584aa5753dd2cc7b3205ae750b3f39a1a73025db
wenfeng.zhu <wenfeng.zhu@geekplus.com>
2021-07-02 14:38:18 +0800

    init project

---
+++ src/main/java/com/auditflow/customize/CustomizeApplication.java
@@ -0,0 +1,13 @@
+package com.auditflow.customize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/auditflow/customize/CustomizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auditflow.customize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
